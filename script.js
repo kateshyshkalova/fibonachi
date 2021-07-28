41c5108a630cc7b9189d5961f160378d40b28a62
kateshyshkalova <77622318+kateshyshkalova@users.noreply.github.com>
2021-07-28 16:56:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+let n = prompt('Скільки чисел послідовності ви хочете дізнатися?');
+
+let fib = n => {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+console.log();
+for (let i = 1; i <= n; i++) {
+    console.log(fib(i));
+}
